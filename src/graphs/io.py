e8e16fb8486586327338f291e08c2ccd4aa085fe
Felipe Matias <74963264+Zibec@users.noreply.github.com>
2025-11-04 11:10:26 -0300

    Feat: função de salvar bairros_unique

--- src/graphs/io.py
+++ src/graphs/io.py
@@ -32,28 +32,41 @@ def derreter_bairros_recife(input_path: str) -> dict[str, str]:
                     if not bairro_norm: #Garante que o nome do bairro não está vazio 
                         raise ValueError(f"Bairro vazio encontrado em microrregião {microrregiao}")
                                 
-                    if bairro_norm in bairros_dict:     #Se o bairro já foi salvo antes em outra microrregião solta um erro 
+                    if bairro_norm in bairros_dict:     #Se o bairro já foi salvo antes em outra microrregião ele não adiciona no dicionário 
                         if bairros_dict[bairro_norm] != microrregiao:
-                            raise ValueError(
-                                f"Conflito: '{bairro_norm}' aparece em {bairros_dict[bairro_norm]} e {microrregiao}"
-                            )
+                            continue
                     else:  #Caso contrário adiciona ao dicioniario 
                         bairros_dict[bairro_norm] = microrregiao
 
         return bairros_dict
 
+def salvar_bairros_unique(bairros_dict: dict[str, str], output_path: str) -> None: #Gera o arquivo bairros_unique.csv a partir do dicionário {bairro: microrregiao}.
+    # Abre (ou cria) o arquivo CSV para escrita
+    with open(output_path, "w", encoding="utf-8", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        
+        # Cabeçalho fixo
+        writer.writerow(["bairro", "microrregiao"])
+        
+        # Escreve cada linha do dicionário
+        for bairro, microrregiao in bairros_dict.items():
+            writer.writerow([bairro, microrregiao])
+
+    print(f"Arquivo salvo com sucesso em: {output_path}")
+
 # Main para testar as funções 
 if __name__ == "__main__":
     caminho_csv = "../../data/bairros_recife.csv"
+    saida_csv = "../../data/bairros_unique.csv"
 
     try:
         bairros = derreter_bairros_recife(caminho_csv)  # Dicionario
-        print(f"✅ Total de bairros únicos: {len(bairros)}\n")
+        print(f"Total de bairros únicos: {len(bairros)}\n")
 
         for i, (bairro, microrregiao) in enumerate(bairros.items()):
             print(f"{bairro} -> {microrregiao}")
-            if i == 9:
-                break
 
     except Exception as e:
         print("Erro:", e)
+
+    salvar_bairros_unique(bairros, saida_csv)
